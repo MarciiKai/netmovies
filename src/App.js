68d3ec65e73c1910ed0859418ea2a76aa63a15a0
MarciiKai <kai.marcii2000@gmail.com>
2023-12-02 13:20:57 +0300

    nav bar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import {Route, Routes} from 'react-router-dom'
 import Home from "./Components/Screens/Home";
 import Movies from "./Components/Screens/Movies";
 import TvShows from "./Components/Screens/TvShows";
+import NetMoviesNavbar from "./Components/Navbar";
+
 function App(){
 
   return(
+    <>
+     <NetMoviesNavbar/>
     <Routes>
       <Route path ='/' element ={<Home/>} />
       <Route path ='/Movies' element = {<Movies/>} />
       <Route path ='/TvShows' element = {<TvShows/>} />
-
-
     </Routes>
+    </>
+   
     
     )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import {React} from 'react'
+import {Nav, Navbar, Container} from 'react-bootstrap';
 
-const Navbar = () => {
+const NetMoviesNavbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">NetMovies</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Movies</Nav.Link>
+          <Nav.Link href="#pricing">TvShows</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>  
+   
+  </>
+);
+};
 
-export default Navbar;
+
+export default NetMoviesNavbar;

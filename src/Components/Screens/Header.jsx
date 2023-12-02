b293f353278ba3bb9bdd3f23ec91058cfe8a6ec9
MarciiKai <kai.marcii2000@gmail.com>
2023-12-02 17:55:47 +0300

    landing page

--- src/Components/Screens/Header.jsx
+++ src/Components/Screens/Header.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 function Header() {
   return (
-    <div id = "main">
+    <header id="main">
+      <div className="header-container">
+        <div className="intro-text">
+          <div className="intro-lead-in">Welcome to NetMovies</div>
+          <div className="intro-heading text-uppercase">
+            NetMovies is among the best online streaming sites for free
+          </div>
+        </div>
 
-    </div>
-  )
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." />
+          <button type="button">Search</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;

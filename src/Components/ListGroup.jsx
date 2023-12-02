d5a6c11a95e2d6107a947b04ea27539d255b91f1
MarciiKai <kai.marcii2000@gmail.com>
2023-12-02 11:57:32 +0300

    First commit

---
+++ src/Components/ListGroup.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+
+function ListGroup( ){
+  let items =[ 'January', 'February', 'March', 'April', 'May']
+  const [selectedIndex ,setSelectedIndex] = useState(-1);
+  
+  return(
+    <>
+      <h1>Months</h1>
+      {items.length === 0 && <p>List not found</p>}
+      <ul className="list-group">
+     {items.map((item, index) =>
+       <li className={selectedIndex === index ?
+         "list-group-item active" : 
+         "list-group-item"} 
+       key= {item}
+       onClick={() => setSelectedIndex(index)
+       }>{item}</li>)} 
+      </ul>
+    </>
+        ) ;
+
+}
+
+
+export default ListGroup;

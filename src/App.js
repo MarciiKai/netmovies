10eb02a74bc514a5c37bd0a32f20499748f370f8
MarciiKai <kai.marcii2000@gmail.com>
2023-12-02 12:18:40 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import './Components/ListGroup'
-import ListGroup from './Components/ListGroup';
-
+// import './App.css';
+import React from "react";
+import {Route, Routes} from 'react-router-dom'
 function App(){
 
   return(
-    <div>
-      <ListGroup></ListGroup>
-    </div>
-  )
+    <Routes>
+
+    </Routes>
+    
+    )
 }
 
 

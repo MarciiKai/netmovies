61529162d2f9903287b87782b3651ddc122ca60d
MarciiKai <kai.marcii2000@gmail.com>
2023-12-05 12:51:31 +0300

    updated

--- src/Components/Screens/Header.jsx
+++ src/Components/Screens/Header.jsx
@@ -15,9 +15,32 @@ function Header() {
           <input type="text" placeholder="Search..." />
           <button type="button">Search</button>
         </div>
-
         <div className="full-site">
           <button type="button" > View Full Site ...</button>
+        </div>
+
+        <div className="footer-text">
+          <div className="footer-lead-in">
+          Explore NetMovies, your premier destination for free online movies and TV series.
+          </div>
+          <div className="footer-heading text-uppercase">Here's why NetMovies stands out:  </div>
+            <div className="footer-list">
+            <ul>
+              <li>Ad-free experience</li>
+              <li>Free and high-speed streaming</li>
+              <li>No account needed</li>
+              <li>One-click streaming convenience</li>
+              <li>Extensive library with over 400,000 videos</li>
+            </ul>
+            </div>
+
+            <div className="last-footer-text">
+            If you're seeking a secure platform for online movie watching, give NetMovies a try. Enjoy our service? Bookmark and share with your friends.
+            </div>
+            <div className="last-text">
+            Thank you for choosing NetMovies!
+            </div>
+
         </div>
 
       </div>

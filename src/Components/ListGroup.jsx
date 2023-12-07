49c1d39b270c453eaa49f18ceeebc673af6d4a98
MarciiKai <kai.marcii2000@gmail.com>
2023-12-07 16:35:29 +0300

    List

--- src/Components/ListGroup.jsx
+++ src/Components/ListGroup.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { useState } from 'react';
+// import React from 'react';
+// import { useState } from 'react';
 
-function ListGroup( ){
-  let items =[ 'January', 'February', 'March', 'April', 'May']
-  const [selectedIndex ,setSelectedIndex] = useState(-1);
+// function ListGroup( ){
+//   let items =[ 'January', 'February', 'March', 'April', 'May']
+//   const [selectedIndex ,setSelectedIndex] = useState(-1);
   
-  return(
-    <>
-      <h1>Months</h1>
-      {items.length === 0 && <p>List not found</p>}
-      <ul className="list-group">
-     {items.map((item, index) =>
-       <li className={selectedIndex === index ?
-         "list-group-item active" : 
-         "list-group-item"} 
-       key= {item}
-       onClick={() => setSelectedIndex(index)
-       }>{item}</li>)} 
-      </ul>
-    </>
-        ) ;
+//   return(
+//     <>
+//       <h1>Months</h1>
+//       {items.length === 0 && <p>List not found</p>}
+//       <ul className="list-group">
+//      {items.map((item, index) =>
+//        <li className={selectedIndex === index ?
+//          "list-group-item active" : 
+//          "list-group-item"} 
+//        key= {item}
+//        onClick={() => setSelectedIndex(index)
+//        }>{item}</li>)} 
+//       </ul>
+//     </>
+//         ) ;
 
-}
+// }
 
 
-export default ListGroup;
+// export default ListGroup;

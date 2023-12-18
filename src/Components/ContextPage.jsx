0411517cade76cd6b5a9669356abfb7a03074b7c
MarciiKai <kai.marcii2000@gmail.com>
2023-12-18 15:27:10 +0300

    Context page updated

--- src/Components/ContextPage.jsx
+++ src/Components/ContextPage.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function contextPage() {
-  return (
-    <div>contextPage</div>
-  )
+const ContextPage = createContext();
+export function MovieProvider({children}){
+
+  const [header, setHeader] = useState("Trending");
+  const [totalPage, setTotalPage] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [searchedMovies, setSearchedMovies] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+  const [page, setPage] = useState(1);
+  const [activegenre, setActiveGenre] = useState(28);
+  const [genres, setGenres] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const navigate = useNavigate();
+
+  const APIKEY = import.meta.env.VITE_API_KEY;
+
 }
-
-export default contextPage;
